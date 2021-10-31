9e8c08bf4945d65c1809f1430e355b6893e90a47
Jonathan Tushman <jtushman@gmail.com>
2021-10-31 08:37:44 -0400

    fixing coverage

--- test/oneWay.spec.ts
+++ test/oneWay.spec.ts
@@ -1,4 +1,4 @@
-import { asyncForEach } from '../src/index';
+import { asyncForEach, asyncTimes } from '../src/index';
 
 export async function sleep(ms = 15_000) {
   return new Promise(resolve => {
@@ -14,6 +14,20 @@ describe('oneWay', () => {
     await asyncForEach(orderedList, async num => {
       await sleep(100); // do something that requires an await
       outputList.push(num);
+    });
+
+    expect(outputList).toEqual(orderedList);
+  });
+
+  it('can loop over something asynchronously _n_ times', async () => {
+    const orderedList = [1, 2, 3, 4, 5];
+    let outputList: number[] = [];
+
+    let counter = 0;
+    await asyncTimes(5, async () => {
+      await sleep(100); // do something that requires an await
+      counter = counter + 1;
+      outputList.push(counter);
     });
 
     expect(outputList).toEqual(orderedList);
